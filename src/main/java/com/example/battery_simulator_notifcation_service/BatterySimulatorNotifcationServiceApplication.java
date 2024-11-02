d6a3be267d6d82bc027f320ead8a17d22d4e0c42
Mark Veerasingam <markveer70@gmail.com>
2024-11-02 23:57:13 +0000

    first commit

---
+++ src/main/java/com/example/battery_simulator_notifcation_service/BatterySimulatorNotifcationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.battery_simulator_notifcation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterySimulatorNotifcationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatterySimulatorNotifcationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/battery_simulator_notifcation_service/BatterySimulatorNotifcationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battery_simulator_notifcation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatterySimulatorNotifcationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
